cc3405f124ae99f7460200dc6a42f66b1f432ebf
sumeyrasrgl <sumeyrasarigull@gmail.com>
2020-10-15 00:57:15 +0300

    Update

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -6,7 +6,14 @@ public class DoorController : MonoBehaviour
 {
 
     public bool isOpen;
-    public float openedRotation,closedRotation,speed;
+    public float openedRotation,closedRotation,speed1,speed2;
+    public GameObject Door2;
+    Transform door;
+    void Start()
+    {
+         door = Door2.GetComponent<Transform>();
+            
+    }
 
     public void MoveDoor()
     {
@@ -15,29 +22,39 @@ public class DoorController : MonoBehaviour
     }
     private void Update()
     {
+        OpenDoor();
+    }
+
+    public void OpenDoor()
+    {
         if (isOpen)
         {
             Quaternion doorRotation1 = Quaternion.Euler(0, openedRotation, 0);
-            transform.localRotation = Quaternion.Slerp(transform.rotation, doorRotation1, speed * Time.deltaTime);
-        }else if (isOpen==false)
+            transform.localRotation = Quaternion.Slerp(transform.rotation, doorRotation1, speed1 * Time.deltaTime);
+            door.localRotation = Quaternion.Slerp(door.rotation, doorRotation1, speed2 * Time.deltaTime);
+
+
+        }
+        else if (isOpen == false)
         {
             Quaternion doorRotation2 = Quaternion.Euler(0, closedRotation, 0);
-            transform.localRotation = Quaternion.Slerp(transform.rotation, doorRotation2, speed * Time.deltaTime);
+            transform.localRotation = Quaternion.Slerp(transform.rotation, doorRotation2, speed1 * Time.deltaTime);
         }
-        
     }
+
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if (isOpen==false)
             {
+               
                 isOpen = true;
-            }else if (isOpen)
-            {
-                isOpen = false;
+ 
             }
-           
+          
+
         }
     }
 
---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public float openedRotationn,closedRotationn,speedd;
+    public bool isOpenn;
+    public void Moved()
+    {
+        isOpenn = !isOpenn;
+    }
+    private void Update()
+    {
+        if (isOpenn==true)
+        {
+            Quaternion doorRotationn1 = Quaternion.Euler(0, openedRotationn, 0);
+            transform.localRotation = Quaternion.Slerp(transform.rotation, doorRotationn1, speedd * Time.deltaTime);
+
+        }
+    }
+
+   
+
+}
