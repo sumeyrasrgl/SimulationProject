d6056503e166d86b9a51d798548f034f24bd7956
sumeyrasrgl <sumeyrasarigull@gmail.com>
2020-10-14 22:23:05 +0300

    Doors Script

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    public bool isOpen;
+    public float openedRotation,closedRotation,speed;
+
+    public void MoveDoor()
+    {
+        isOpen = !isOpen;
+    }
+    private void Update()
+    {
+        if (isOpen)
+        {
+            Quaternion doorRotation1 = Quaternion.Euler(0, openedRotation, 0);
+            transform.localRotation = Quaternion.Slerp(transform.rotation, doorRotation1, speed * Time.deltaTime);
+        }
+       /* else
+        {
+            Quaternion doorRotation2 = Quaternion.Euler(0, closedRotation, 0);
+            transform.localRotation = Quaternion.Slerp(transform.rotation, doorRotation2, speed * Time.deltaTime);
+        }*/
+    }
+}
